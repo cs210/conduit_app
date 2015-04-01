416e83515efd30fb16a67f31449a5a6e049fafa5
nmasharani <nisham@stanford.edu>
2015-04-01 11:52:40 -0700

    custom message view done

--- conduit/conduit/CustomMessageController.swift
+++ conduit/conduit/CustomMessageController.swift
@@ -2,7 +2,7 @@
 //  CustomMessageController.swift
 //  conduit
 //
-//  Created by Nisha Masharani on 3/31/15.
+//  Created by Nisha Masharani on 4/1/15.
 //  Copyright (c) 2015 Conduit. All rights reserved.
 //
 
@@ -11,10 +11,27 @@ import UIKit
 
 class CustomMessageController : UIViewController {
   
-  @IBOutlet weak var textArea: UITextField!
+  @IBOutlet weak var licenseTextField: UITextField!
+  @IBOutlet weak var messageTextField: UITextField!
+  var licensePlate : String!
   
   override func viewDidLoad() {
-    textArea.selected = true
+    super.viewDidLoad()
+    messageTextField.becomeFirstResponder()
+    licenseTextField.text = licensePlate
   }
   
+  // Send a custom message 
+  // TODO: add API calls
+  @IBAction func sendCustomMessage(sender: AnyObject) {
+    // TODO: do we want to trim just whitespace or whitespace AND newlines?
+    var message = messageTextField.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+    
+    // if we have an empty message, do nothing.
+    if message == "" {
+      return
+    }
+    
+    // TODO: API calls here.
+  }
 }
--- conduit/conduit/NewMessageViewController.swift
+++ conduit/conduit/NewMessageViewController.swift
@@ -11,29 +11,36 @@ import UIKit
 
 class NewMessageViewController : UIViewController, UITableViewDataSource {
   // Init selected message to "" because  you can't send an empty message
-  
   var selectedMessage = ""
   var selectedMessageIndexPath : NSIndexPath?
-  
   var presetMessages = [
     "Could you please unlock your charging port? Thank you!",
     "When will you be back to your car?",
     "Move your car now or else."
   ]
+  // fake data for testing
+  var fakeLicensePlate = "ABC123"
 
+  @IBOutlet weak var licenseTextField: UITextField!
   @IBOutlet weak var presetTable: UITableView!
   
+  // These variables make sure that tapping works as expected
+  // i.e. when you tap anywhere when keyboard is enabled, it is dismissed
+  // i.e. when you tap to select a table cell, tapping elsewhere deselects it
   @IBOutlet var keyboardDismisser: UITapGestureRecognizer!
 
   @IBOutlet var presetDeselecter: UITapGestureRecognizer!
   
-  @IBAction func enableKeyboardDismisser(sender: AnyObject) {
-    keyboardDismisser.enabled = true
-  }
-  
   override func viewDidLoad() {
     keyboardDismisser.enabled = false
     presetDeselecter.enabled = false
+    licenseTextField.text = fakeLicensePlate
+  }
+  
+  // These functions ensure correct tapping to dismiss keyboard and to deselect
+  // table cells
+  @IBAction func enableKeyboardDismisser(sender: AnyObject) {
+    keyboardDismisser.enabled = true
   }
   
   @IBAction func dismissKeyboard(sender: AnyObject) {
@@ -50,6 +57,7 @@ class NewMessageViewController : UIViewController, UITableViewDataSource {
     }
   }
   
+  // These functions manage the preset message list.
   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return presetMessages.count
   }
@@ -68,8 +76,31 @@ class NewMessageViewController : UIViewController, UITableViewDataSource {
     selectedMessageIndexPath = indexPath
     presetDeselecter.enabled = true
   }
+  
+  // This function ensures that data from this view (i.e. license plate) is sent
+  // correctly to the custom message view.
+  
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if segue.identifier == "customMessageSegue" {
+      var next = segue.destinationViewController as CustomMessageController
+      next.licensePlate = licenseTextField.text
+    }
+  }
+  
+  // This is the function to actually send a message. TODO: include API calls.
+  @IBAction func sendMessage(sender: AnyObject) {
+    // if there's no selected custom message, the send button does nothing.
+    if selectedMessageIndexPath == nil {
+      return
+    }
+    
+    // TODO: send the message here.
+    
+  }
 }
 
+// We have a custom table cell which contains a label, so that we can customize
+// the look and positioning of table cells.
 class NewMessageTableViewCell : UITableViewCell {
   @IBOutlet weak var label: UILabel!
 }
